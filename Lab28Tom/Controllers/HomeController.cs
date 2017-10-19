67b8f1cb3c931e3379e1f44edf689be233552f10
Thomas Reyes <thomasellison.r@gmail.com>
2017-10-19 11:53:52 -0700

    home controller and view set up

---
+++ Lab28Tom/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Lab28Tom.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab28Tom.Controllers
+{
+    public class HomeController : Controller
+    {
+        ////private field, used for Dependendcy Injection
+        //private readonly Lab28TomContext _context;
+
+        ////Constructor for Home Controller - a DBContext is required
+        //public HomeController(Lab28TomContext context)
+        //{
+        //    _context = context;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    // Calling to the LFG table in the database and getting the content for ID 1
+        //    var result = _context.LFG.Where(c => c.ID == 1);
+
+        //    return View(result.ToList());
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
