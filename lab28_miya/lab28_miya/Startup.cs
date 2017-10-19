51524214f28f96fb7d9ec05d57f76cd16eabb315
miyaluc <srlucas3@gmail.com>
2017-10-19 11:01:42 -0700

    CPS controller added

--- lab28_miya/lab28_miya/Startup.cs
+++ lab28_miya/lab28_miya/Startup.cs
@@ -6,16 +6,31 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using lab28_miya.Models;
 
 namespace lab28_miya
 {
     public class Startup
     {
+        public IConfiguration Configuration
+        {
+            get;
+        }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<lab28_miyaContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("lab28_miyaContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
