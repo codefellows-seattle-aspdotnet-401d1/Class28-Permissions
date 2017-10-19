5afc4eef8eb8fdd38aae47da12d4e89c8751618e
Thomas Reyes <thomasellison.r@gmail.com>
2017-10-19 13:06:45 -0700

    HomeController updated

--- Lab28Tom/Controllers/HomeController.cs
+++ Lab28Tom/Controllers/HomeController.cs
@@ -9,25 +9,21 @@ namespace Lab28Tom.Controllers
 {
     public class HomeController : Controller
     {
-        ////private field, used for Dependendcy Injection
-        //private readonly Lab28TomContext _context;
+        //private field, used for Dependendcy Injection
+        private readonly Lab28TomContext _context;
 
-        ////Constructor for Home Controller - a DBContext is required
-        //public HomeController(Lab28TomContext context)
-        //{
-        //    _context = context;
-        //}
+        //Constructor for Home Controller - a DBContext is required
+        public HomeController(Lab28TomContext context)
+        {
+            _context = context;
+        }
 
-        //public IActionResult Index()
-        //{
-        //    // Calling to the LFG table in the database and getting the content for ID 1
-        //    var result = _context.LFG.Where(c => c.ID == 1);
-
-        //    return View(result.ToList());
-        //}
         public IActionResult Index()
         {
-            return View();
+            // Calling to the LFG table in the database and getting the content for ID 1
+            var result = _context.LFG.Where(c => c.ID == 1);
+
+            return View(result.ToList());
         }
     }
 }
