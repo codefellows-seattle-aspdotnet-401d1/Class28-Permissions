ff5a98ac2e21808c4607f71d616f2c82690b2de4
Gregory Dukes <kcils360@gmail.com>
2017-10-19 10:09:31 -0700

    added post and get for register in account controller

--- lab28Permissions/lab28Permissions/Controllers/AccountController.cs
+++ lab28Permissions/lab28Permissions/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using lab28Permissions.Models;
+using System.Security.Claims;
 
 namespace lab28Permissions.Controllers
 {
@@ -19,8 +20,33 @@ namespace lab28Permissions.Controllers
             _signInManager = signInManager;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Register(string returnUrl = null)
         {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel rvm, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = rvm.Email, Email = rvm.Email };
+                var result = await _userManager.CreateAsync(user, rvm.Password);
+
+                if (result.Succeeded)
+                {
+                    var addRole = await _userManager.AddClaimAsync(user, (new Claim(ClaimTypes.Role, "Administrator", ClaimValueTypes.String)));
+                    if (addRole.Succeeded)
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
             return View();
         }
     }
