363ebb3efd3708cd48e84a7ac991c356347b2972
Austin Cohrs <acohrsdev@outlook.com>
2017-10-19 10:28:05 -0700

    Adding admin register actions to CrewAuthController.

---
+++ IdentityDay2/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using IdentityDay2.Models;
+using IdentityDay2.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityDay2
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDbContext<IdentityDay2Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("IdentityDay2Context")));
+
+            // This context is derived from IdentityDbContext. This context is responsible for the ASPNET Identity tables in the database. 
+            services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("IdentityDay2Context")));
+
+            services.AddIdentity<CrewMember, IdentityRole>()
+                   .AddEntityFrameworkStores<AppDbContext>()
+                   .AddDefaultTokenProviders();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Login to Create Announcements.");
+            });
+        }
+    }
+}
