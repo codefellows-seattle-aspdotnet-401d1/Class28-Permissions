4332f8b75480b8f9bcffd7546ce26cdf75cc10ea
Brandon Little <brlittle86@gmail.com>
2017-10-20 06:43:51 -0700

    split Register into another action for Administrator registration

--- DnDManager/DnDManager/Controllers/AccountController.cs
+++ DnDManager/DnDManager/Controllers/AccountController.cs
@@ -38,6 +38,32 @@ namespace DnDManager.Controllers
 
                 if (result.Succeeded)
                 {
+                    await _signInManager.PasswordSignInAsync(rvm.Email, rvm.Password, true, true);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AdminRegister(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AdminRegister(RegisterViewModel rvm, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = rvm.Email, Email = rvm.Email, Birthday = rvm.Birthday };
+                var result = await _userManager.CreateAsync(user, rvm.Password);
+
+                if (result.Succeeded)
+                {
                     List<Claim> myClaims = new List<Claim>();
 
                     Claim admin = new Claim(ClaimTypes.Role, "Administrator", ClaimValueTypes.String);
