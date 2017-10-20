8315da37387cabcd24a2c8dd179551b0887825cb
Brandon Little <brlittle86@gmail.com>
2017-10-20 06:52:18 -0700

    added policy for administrator access within the site

--- DnDManager/DnDManager/Startup.cs
+++ DnDManager/DnDManager/Startup.cs
@@ -22,6 +22,9 @@ namespace DnDManager
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthorization(options => 
+                     options.AddPolicy("Admin Only", policy => policy.RequireRole("Administrator")));
+
             services.AddMvc();
 
             services.AddDbContext<DnDManagerContext>(options =>
